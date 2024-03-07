1ce86daa317924553b75f1938206a82d45fe34a6
Renee <156127959+reneexie01@users.noreply.github.com>
2024-03-08 07:08:14 +1100

    02_repeatString completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+let text = '';
+const repeatString = function(str, n) {
+    if (n < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < n; i++) {
+        text += str;
+    }
+    return text;
 };
 
 // Do not edit below this line
