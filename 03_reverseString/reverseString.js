0186d88bd68d25efa805efc82623648ed75de6ed
Renee <156127959+reneexie01@users.noreply.github.com>
2024-03-08 15:18:36 +1100

    03_reverseString completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitStr = str.split("");
+    let reversal = splitStr.reverse();
+    let reversedStr = reversal.join("");
+    return reversedStr;
 };
 
 // Do not edit below this line
