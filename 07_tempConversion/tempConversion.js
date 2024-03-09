8d32622a9b1cfdb8f7b65c4c0b69335ff66382b1
Renee <156127959+reneexie01@users.noreply.github.com>
2024-03-10 07:14:14 +1100

    07_tempConversion completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+    const formula = (F - 32) * (5/9);
+    const twoDecimal = formula.toFixed(1);
+    const cel = Number(twoDecimal);
+    return cel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+    const formula = (C * (9/5) + 32);
+    const twoDecimal = formula.toFixed(1);
+    const fah = Number(twoDecimal);
+    return fah;
 };
 
 // Do not edit below this line
