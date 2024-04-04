3278924bb27796963a26d15668e552d0835b1f60
Renee <156127959+reneexie01@users.noreply.github.com>
2024-04-05 07:05:09 +1100

    09_palindromes completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const withoutSpecChar = str.toLowerCase().replace(/[\s~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=-]/g, '');
+    //const withoutNums = withoutSpecChar.replace(/[0-9]/g, '');
+    const array = withoutSpecChar.split('');
+    let reversed = [];
+    for (let i = array.length; i >= 0; i--) {
+        reversed.push(array[i]);
+    }
+    const palin = reversed.join('');
+    const original = array.join('');
+    if (palin === original) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
