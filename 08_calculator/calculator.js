f996944c10dd95066bb24e1c852dafb81e4defe4
Renee <156127959+reneexie01@users.noreply.github.com>
2024-04-05 06:17:11 +1100

    08_calculator completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, index) => (total + index), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, index) => (total * index), 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = 1;
+  for (let i = a; i > 1; i--) {
+    if (a == 0 || a == 1) {
+      return 1;
+    } else {
+      result *= i
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
