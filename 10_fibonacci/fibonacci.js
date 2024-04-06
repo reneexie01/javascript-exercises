00edeaf823d465e7cbb6357192a229789f5dfdb0
Renee <156127959+reneexie01@users.noreply.github.com>
2024-04-06 17:52:35 +1100

    10_fibonacci completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num == 0 || num == null) {
+        return 0;
+    } else if (num < 0) {
+        return 'OOPS';
+    } else {
+        let n1 = 0;
+        let n2 = 1;
+        let n3;
+        let array = [];
+    for (let i = 0; i <= num; i++) {
+        n3 = n1 + n2;
+        n1 = n2;
+        n2 = n3;
+        array.push(n1);
+    }
+    return array[num - 1];
+    }
 };
 
 // Do not edit below this line
