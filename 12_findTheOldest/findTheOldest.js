f9104e3d88d2f9b6a94c60be7d940169566322f3
Renee <156127959+reneexie01@users.noreply.github.com>
2024-04-07 12:25:08 +1000

    12_findTheOldest completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    array.map((person) => {
+        let age;
+        if (person.yearOfDeath === undefined) {
+            let currentYear = (new Date()).getFullYear();
+            age = currentYear - person.yearOfBirth;
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        person.age = age;
+    })
+    array.sort((a, b) => {
+        if (a.age - b.age > 0) {
+            return -1
+        } else if (a.age - b.age < 0) {
+            return 1
+        } else {
+            return 0;
+        }
+    })
+    return array[0];
 };
 
 // Do not edit below this line
